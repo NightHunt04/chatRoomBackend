a11c918ec8f218f02834f2e96f211bfafe25d6da
admin1334 <jeetbherwani2004@gmail.com>
2024-07-14 12:56:58 +0530

    Issue

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ io.on('connection', (socket) => {
     })
 })
 
-module.exports = app
-// module.exports = server
+// module.exports = app
+module.exports = server
 
 server.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`)

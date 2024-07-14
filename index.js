e03edfba68aabe5e2db0d01776f0cf03693b2cf9
admin1334 <jeetbherwani2004@gmail.com>
2024-07-14 12:47:27 +0530

    Issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -26,11 +26,7 @@ function getTime() {
     return `${hours}:${minutes} ${ampm}`;
 }
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true
-}))
+app.use(cors())
 
 let connectedClientsCount = {}
 
